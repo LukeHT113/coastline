1f7062506eddb2668275cdd73d6501fec1b7a595
Luke Talling <luketalling@outlook.com>
2024-06-14 12:39:13 +0100

    Updated heading-with-hero

--- patterns/heading-with-hero.php
+++ patterns/heading-with-hero.php
@@ -13,8 +13,8 @@
   <div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
   <!-- /wp:spacer -->
 
-  <!-- wp:image {"id":570,"sizeSlug":"full","linkDestination":"none","className":"is-style-default rounded"} -->
-  <figure class="wp-block-image size-full is-style-default rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/beach-view.avif" alt="" class="wp-image-570" style="aspect-ratio:16/9;object-fit:cover" /></figure>
+  <!-- wp:image {"id":570,"aspectRatio":"16/9","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-default rounded"} -->
+  <figure class="wp-block-image size-full is-style-default rounded"><img src="http://sophie-talling-sportswear.local/wp-content/themes/coastline/assets/images/beach-view.avif" alt="" class="wp-image-570" style="aspect-ratio:16/9;object-fit:cover"/></figure>
   <!-- /wp:image -->
 </div>
 <!-- /wp:group -->
